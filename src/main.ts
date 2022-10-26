14bcd382be1aff616e311a48940b795282441f4b
Marsboy02 <marsboy0619@gmail.com>
2022-10-26 21:47:45 +0900

    feat: swagger docs 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import generateSwaggerDocument from './infrastructure/swagger/swagger.generator';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  SwaggerModule.setup('docs', app, generateSwaggerDocument(app));
   await app.listen(3000);
 }
 bootstrap();
