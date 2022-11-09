08ca7e44df77f8e1ddca8a2b4d5ebcefb659eca2
Marsboy02 <marsboy0619@gmail.com>
2022-11-09 13:14:44 +0900

    feat: axios를 통한 html 크롤링 기능 추가

--- src/app/crawler/card.service.ts
+++ src/app/crawler/card.service.ts
@@ -30,7 +30,8 @@ export class CardService {
 
   async getHTML(name: string) {
     try {
-      return await this.axios.get(url, params.keyword[name]);
+      params.keyword = name;
+      return await this.axios.get(url, { params });
     } catch (error) {
       console.log(error);
     }
@@ -39,9 +40,15 @@ export class CardService {
   getCardData(name: string) {
     this.getHTML(name).then((html) => {
       const $ = this.cheerio.load(html.data);
+      console.log(html);
+      const card_img = $('#card_list > div > div');
+      const card_effect = $(
+        '#card_list > div > dl > dd.box_card_text.c_text.flex_1',
+      ).text();
     });
+
     return name;
   }
 }
-
-// https://www.db.yugioh-card.com/yugiohdb/card_search.action?ope=1&sess=1&rp=10&mode=&sort=1&keyword=검투수 라크엘&stype=1&ctype=&othercon=2&starfr=&starto=&pscalefr=&pscaleto=&linkmarkerfr=&linkmarkerto=&link_m=2&atkfr=&atkto=&deffr=&defto=https://www.db.yugioh-card.com/yugiohdb/card_search.action?ope=1&sess=1&rp=10&mode=&sort=1&keyword=검투수 라크엘&stype=1&ctype=&othercon=2&starfr=&starto=&pscalefr=&pscaleto=&linkmarkerfr=&linkmarkerto=&link_m=2&atkfr=&atkto=&deffr=&defto=
+// https://www.db.yugioh-card.com/yugiohdb/card_search.action?ope=1&sess=1&rp=10&mode=&sort=1&keyword=검투수 라크엘&stype=1&ctype=&othercon=2&starfr=&starto=&pscalefr=&pscaleto=&linkmarkerfr=&linkmarkerto=&link_m=2&atkfr=&atkto=&deffr=&defto=
+// https://www.db.yugioh-card.com/yugiohdb/card_search.action?ope=1&sess=1&rp=10&mode=&sort=1&keyword=검투수 라크엘&stype=1&ctype=&othercon=2&starfr=&starto=&pscalefr=&pscaleto=&linkmarkerfr=&linkmarkerto=&link_m=2&atkfr=&atkto=&deffr=&defto=
