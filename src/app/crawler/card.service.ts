3e19e7232bc22bdaadf1cbc56b42719c35cc90c4
Marsboy02 <marsboy0619@gmail.com>
2022-11-09 12:06:30 +0900

    feat: axios url 및 파라미터 설정

--- src/app/crawler/card.service.ts
+++ src/app/crawler/card.service.ts
@@ -1,9 +1,47 @@
 import { Injectable } from '@nestjs/common';
 
+const url = `https://www.db.yugioh-card.com/yugiohdb/card_search.action`;
+const params = {
+  ope: 1,
+  sess: 1,
+  rp: 10,
+  mode: null,
+  keyword: null,
+  stype: 1,
+  ctype: null,
+  othercon: 2,
+  starfr: null,
+  starto: null,
+  pscalefr: null,
+  pscaleto: null,
+  linkmarkerfr: null,
+  linkmarkerto: null,
+  link_m: 2,
+  atkfr: null,
+  atkto: null,
+  deffr: null,
+  defto: null,
+};
+
 @Injectable()
 export class CardService {
   cheerio = require('cheerio');
+  axios = require('axios');
+
+  async getHTML(name: string) {
+    try {
+      return await this.axios.get(url, params.keyword[name]);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   getCardData(name: string) {
+    this.getHTML(name).then((html) => {
+      const $ = this.cheerio.load(html.data);
+    });
     return name;
   }
 }
+
+// https://www.db.yugioh-card.com/yugiohdb/card_search.action?ope=1&sess=1&rp=10&mode=&sort=1&keyword=검투수 라크엘&stype=1&ctype=&othercon=2&starfr=&starto=&pscalefr=&pscaleto=&linkmarkerfr=&linkmarkerto=&link_m=2&atkfr=&atkto=&deffr=&defto=https://www.db.yugioh-card.com/yugiohdb/card_search.action?ope=1&sess=1&rp=10&mode=&sort=1&keyword=검투수 라크엘&stype=1&ctype=&othercon=2&starfr=&starto=&pscalefr=&pscaleto=&linkmarkerfr=&linkmarkerto=&link_m=2&atkfr=&atkto=&deffr=&defto=
